3e0c6b0047246b0f74d3033df37c493eaacf460c
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-01 00:39:57 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -14,9 +14,9 @@ namespace Statistics.Test
             List<float> numbers = new List<float>() { 1.5f, 8.9f, 3.2f, 4.5f};
             var computedStats = statsComputer.CalculateStatistics(numbers);
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.Average- 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.Max- 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.Min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Average- 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Max- 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Min - 1.5) <= epsilon);
         }
         public void ReportsNaNForEmptyInput()
         {
