118a7695af98f67027bfe5f9c35214a027e3c607
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-01 00:18:41 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public float Average { get;  set; }
+        public float Max { get; set; }
+        public float Min { get; set; }
+
+
+
+        public StatsComputer CalculateStatistics(List<float> numbers) {
+            StatsComputer stats = new StatsComputer();
+            float avg = numbers.Average();
+            float max = numbers.Max();
+            float min = numbers.Min();
+            stats.Average = avg;
+            stats.Max = max;
+            stats.Min = min;
+            return stats;
         }
     }
 }
