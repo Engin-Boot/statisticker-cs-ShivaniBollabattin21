574dd36ce7c89568688301a98e5025970b3a896c
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-03 14:47:35 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -7,20 +7,23 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public float Average { get;  set; }
-        public float Max { get; set; }
-        public float Min { get; set; }
-
-
-
-        public StatsComputer CalculateStatistics(List<float> numbers) {
-            StatsComputer stats = new StatsComputer();
-            float avg = numbers.Average();
-            float max = numbers.Max();
-            float min = numbers.Min();
-            stats.Average = avg;
-            stats.Max = max;
-            stats.Min = min;
+        StatsComputer stats = new StatsComputer();
+            if(numbers.Count ==0)
+            {
+                stats.Average = float.NaN;
+                stats.Max = float.NaN;
+                stats.Min = float.NaN;
+            }
+            else
+            {
+                float avg = numbers.Average();
+                float max = numbers.Max();
+                float min = numbers.Min();
+                stats.Average = avg;
+                stats.Max = max;
+                stats.Min = min;
+                
+            }
             return stats;
         }
     }
