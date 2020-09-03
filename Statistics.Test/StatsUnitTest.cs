d567b1d196422e5ff5091e565f07e317cb7b66ce
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-03 14:58:13 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -23,6 +23,10 @@ namespace Statistics.Test
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<float>{});
+            Assert.True(computedStats.Average == float.NaN);
+            Assert.True(computedStats.Max == float.NaN);
+            Assert.True(computedStats.Min == float.NaN);
+
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
