38f3e6feac4d9f801ba33c21b7065b2c3b3ff4f9
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-04 14:56:58 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -8,7 +8,7 @@ namespace Statistics.Test
     public class StatsUnitTest
     {
         [Fact]
-        public void ReportsAverageMinMax()
+        public void TestAverageMinMaxForAListOfFloatValues()
         {
             var statsComputer = new StatsComputer();
             List<float> numbers = new List<float>() { 1.5f, 8.9f, 3.2f, 4.5f};
@@ -31,6 +31,31 @@ namespace Statistics.Test
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+        }
+        [Fact]
+        public void ReportNaNForListHavingAllNaNElements()
+        {
+            var statsComputer = new StatsComputer();
+            var computedStats = statsComputer.CalculateStatistics(
+                new List<float> { float.NaN,float.NaN});
+            Assert.True(float.IsNaN(computedStats.Average));
+            Assert.True(float.IsNaN(computedStats.Max));
+            Assert.True(float.IsNaN(computedStats.Min));
+            
+
+        }
+        [Fact]
+        public void TestForListHavingSomeElementsAsNaN()
+        {
+            var statsComputer = new StatsComputer();
+            var computedStats = statsComputer.CalculateStatistics(
+                new List<float> { float.NaN, 1.5F, 8.9F, 3.2F, 4.5F });
+            float epsilon = 0.001F;
+            Assert.True(float.IsNaN(computedStats.Average));
+            Assert.True(Math.Abs(computedStats.Max - 8.9) <= epsilon);
+            Assert.True(float.IsNaN(computedStats.Min));
+            
+
         }
     }
 }
